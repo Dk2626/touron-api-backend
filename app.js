d0a6c2cf1e5d7d9db7a2d43d50a022247ecbd513
Mani1211 <manivasagam.shanmugam@touron.in>
2020-07-28 21:12:37 +0530

    dependency added

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
+require("dotenv").config();
 require("./models/City");
 require("./models/Place");
-require('./models/Country')
-require('./models/TourIdea')
+require("./models/Country");
+require("./models/TourIdea");
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors')
+const cors = require("cors");
 const mongoose = require("mongoose");
 const cityRoute = require("./routes/cityRoutes");
 const PlaceRoute = require("./routes/placeRoute");
-const CountryRoute = require('./routes/countryRoutes')
-const TourIdeaRoute = require('./routes/tourIdeaRoute')
+const CountryRoute = require("./routes/countryRoutes");
+const TourIdeaRoute = require("./routes/tourIdeaRoute");
 const app = express();
 
-
 // for production only
-const helmet = require('helmet')
-const compression = require('compression')
+const helmet = require("helmet");
+const compression = require("compression");
 
-app.use(helmet())
-app.use(compression())
+app.use(helmet());
+app.use(compression());
 
-const corsObj={
-  orgin:"http:localost:4200",
-  optionSuccessStatus:200
-}
+const corsObj = {
+  orgin: "http:localost:4200",
+  optionSuccessStatus: 200,
+};
 //using the cors obj
 app.use(cors(corsObj));
 
 app.use(bodyParser.json());
 app.use(cityRoute);
 app.use(PlaceRoute);
-app.use(CountryRoute)
-app.use(TourIdeaRoute)
-const mongodbUri =
-  "mongodb+srv://vicky:aamecvicky123@cluster0-knxey.mongodb.net/city?retryWrites=true&w=majority";
+app.use(CountryRoute);
+app.use(TourIdeaRoute);
+const mongodbUri = process.env.DATABASE;
 
 mongoose.connect(mongodbUri, {
   useNewUrlParser: true,
@@ -50,7 +49,7 @@ mongoose.connection.on("error", (err) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Welcome to touron api")
+  res.send("Welcome to touron api");
   console.log("Hello world");
 });
 app.listen(process.env.PORT || 3000, () => console.log("Running on 3000"));
