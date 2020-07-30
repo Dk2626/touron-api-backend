ebe38cda7a3ac69399ea8e87bdaf6e2d635f4bd7
Mani1211 <manivasagam.shanmugam@touron.in>
2020-07-30 20:07:33 +0530

    date format changed

--- models/Country.js
+++ models/Country.js
@@ -1,67 +1,62 @@
-const  mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const Country = new Schema({
-countryName:{
-    type:String,
-    required:true
-},
-aboutCountry:{
-    type:String,
-    required:true
-},
-imageUrl:{
-    type:String,
-    required:true
-},
-weather:{
-    type:String,
-    required:true
-},
-visa:{
-    onArrival:{
-        type:String,
-        require:true},
-    cost:{
-        type:Number,
-        required:true
-    }
-},
-general:{
-    currency:{
-        type:String,
-        required:true
+const Country = new Schema(
+  {
+    countryName: {
+      type: String,
+      required: true,
     },
-    timeZone:{
-        type:String,
-        required:true
+    aboutCountry: {
+      type: String,
+      required: true,
     },
-    bestTimeToVisit:{
-        type:Array,
-        required:true
-    }
-},
-bestPlaces:{
-    type:String,
-    required:true
-},
-idealDays:{
-    type:String,
-    required:true
-}
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    weather: {
+      type: String,
+      required: true,
+    },
+    visa: {
+      onArrival: {
+        type: String,
+        require: true,
+      },
+      cost: {
+        type: Number,
+        required: true,
+      },
+    },
+    general: {
+      currency: {
+        type: String,
+        required: true,
+      },
+      timeZone: {
+        type: String,
+        required: true,
+      },
+      bestTimeToVisit: {
+        type: Array,
+        required: true,
+      },
+    },
+    bestPlaces: {
+      type: String,
+      required: true,
+    },
+    idealDays: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: { currentTime: () => Date.toLocaleTimeString("en-IN") } }
+);
 
-
-},{timestamps:true})
-
-
-module.exports = mongoose.model('Country',Country)
-
-
-
-
-
-
+module.exports = mongoose.model("Country", Country);
 
 // {
 //     "countryName":"Paris",
--- models/TourIdea.js
+++ models/TourIdea.js
@@ -1,117 +1,113 @@
-const  mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const TourIdea = new Schema({
-
-cityName:{
-    type:String,
-    required:true
-},
-countryName:{
-    type:String,
-    required:true
-},
-
-tourName:{
-    type:String,
-    required:true
-},
-aboutTour:{
-    type:String,
-    required:true
-},
-imageUrl:{
-    type:String,
-    required:true
-},
-tourCost:{
-    adult:{
-        type:Number,
-        required:true
+const TourIdea = new Schema(
+  {
+    cityName: {
+      type: String,
+      required: true,
     },
-    children:{
-        type:Number,
-        required:true
-    }
-},
-ratings:{
-    type:String,
-    require:true
-},
-reviews:{
-    type:String,
-    require:true
-},
-inclusion:{
-    type:String,
-    required:true,
-    maxlength:10000
-},
-itinerary:{
-    type:String,
-    required:true,
-    maxlength:10000
-},
-pickUpPoint:{
-    type:Array,
-    required:true
-},
-tourDuration:{
-    type:String,
-    required:true
-},
-tourCategory:{
-   type:Array,
-   required:true
-},
-tourType:{
-    type:String,
-    require:true
-},
-idealType:{
-   type:Array,
-   required:true
-},
-tourPreferance:{
-   type:String,
-   required:true
-},
-additionalInformation:{
-   type:String,
-   maxlength:10000,
-   required:true
-},
-referanceLink:{
-    type:String,
-    required:true,
-    maxlength:10000
- },
-trending:{
-    type:String,
-    required:true
- },
-pickUpAvailableOn:{
-    type:Array,
-    required:true
- },
-pickUpTime:{
-    type:String,
-    required:true
- },
-dropTime:{
-    type:String,
-    required:true
- }
+    countryName: {
+      type: String,
+      required: true,
+    },
 
+    tourName: {
+      type: String,
+      required: true,
+    },
+    aboutTour: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    tourCost: {
+      adult: {
+        type: Number,
+        required: true,
+      },
+      children: {
+        type: Number,
+        required: true,
+      },
+    },
+    ratings: {
+      type: String,
+      require: true,
+    },
+    reviews: {
+      type: String,
+      require: true,
+    },
+    inclusion: {
+      type: String,
+      required: true,
+      maxlength: 10000,
+    },
+    itinerary: {
+      type: String,
+      required: true,
+      maxlength: 10000,
+    },
+    pickUpPoint: {
+      type: Array,
+      required: true,
+    },
+    tourDuration: {
+      type: String,
+      required: true,
+    },
+    tourCategory: {
+      type: Array,
+      required: true,
+    },
+    tourType: {
+      type: String,
+      require: true,
+    },
+    idealType: {
+      type: Array,
+      required: true,
+    },
+    tourPreferance: {
+      type: String,
+      required: true,
+    },
+    additionalInformation: {
+      type: String,
+      maxlength: 10000,
+      required: true,
+    },
+    referanceLink: {
+      type: String,
+      required: true,
+      maxlength: 10000,
+    },
+    trending: {
+      type: String,
+      required: true,
+    },
+    pickUpAvailableOn: {
+      type: Array,
+      required: true,
+    },
+    pickUpTime: {
+      type: String,
+      required: true,
+    },
+    dropTime: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: { currentTime: () => Date.toLocaleTimeString("en-IN") } }
+);
 
-
-},{timestamps:true})
-
-
-module.exports = mongoose.model('TourIdea',TourIdea)
-
-
+module.exports = mongoose.model("TourIdea", TourIdea);
 
 // {
 //     "cityName":"Paris",
